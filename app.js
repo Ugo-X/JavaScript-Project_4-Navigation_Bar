ec50853b86db5d022b9fa6c57ed69230f990ebda
Ugo-X <ugonnadike001@gmail.com>
2023-09-23 12:40:24 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const getElement = (selector) => {
-  const el = document.querySelector(selector)
-  if (el) return el
-  throw new Error(`Please check your classes : ${selector} does not exist`)
-}
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-const navToggle = getElement('.nav-toggle')
-const links = getElement('.links')
-
-navToggle.addEventListener('click', function () {
-  links.classList.toggle('show-links')
-})
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList.contains('random'));
+  // console.log(links.classList.contains('links'));
+  // so here if we have the show-links class with the original height of the links, we are going to remove it. if not, we are going to add it
+  // if(links.classList.contains('show-links')){
+  //   links.classList.remove("show-links");
+  // }
+  // else{
+  //   links.classList.add('show-links')
+  // }
+  // to avoid writing so many lines of code, we can simply use the toggle function to get this done.
+  links.classList.toggle("show-links");
+});
